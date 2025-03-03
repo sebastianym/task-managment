270cba85b5fef8f38ef0621ee6fdc9b8af3b607c
Sebastián Yepes <syepesm05@gmail.com>
2025-03-02 19:50:24 -0500

    fix: cookie domain

--- src/lib/config/auth/cookiesConfig.ts
+++ src/lib/config/auth/cookiesConfig.ts
@@ -1,8 +1,8 @@
 export const config = {
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // Date.now() + 1 día en milisegundos
   path: "/",
-  domain: "localhost",
-  //domain: process.env.HOST,
+  //domain: "localhost",
+  domain: process.env.HOST,
   httpOnly: true,
   sameSite: "lax" as "lax",
   secure: true,
